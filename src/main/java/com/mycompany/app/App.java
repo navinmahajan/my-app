36c8ca4260007acfa92f178026eb980edd23bc63
navinmahajan <navinsmahajan@gmail.com>
2025-08-18 10:53:10 +0000

    Jenkins practical

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,6 @@ package com.mycompany.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World from AWS & I am learning java and Maven !!!!");
+        System.out.println("Hello World from AWS & I am learning java and Maven and will practice!!!!");
     }
 }
